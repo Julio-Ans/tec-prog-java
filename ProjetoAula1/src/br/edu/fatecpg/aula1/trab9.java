c50fd0b5034f8066093f769658037cebc4669499
julio <julio@JAM>
2024-02-22 18:50:06 -0300

    update- pos aula

--- ProjetoAula1/src/br/edu/fatecpg/aula1/trab9.java
+++ ProjetoAula1/src/br/edu/fatecpg/aula1/trab9.java
@@ -9,6 +9,7 @@ public class trab9 {
 		Scanner ler = new Scanner(System.in);
 		
 		String[] nomes = new String[5]; 
+		int repetido=0;
 		
 		for(int i=0; i<5; i++) {
 		System.out.println("Digite o nome numero "+(i+1)+" :");
@@ -23,17 +24,20 @@ public class trab9 {
 			String teste= nomes[j];
 
 			if(verifica.equals(teste)){
-				System.out.println("O sexto nome digitado está presente na lista e é o "+(j+1)+" nome");
-				
-			}else if(j==4 && verifica.equals(teste)){
-				System.out.println("O nome digitado não está na lista");
-
-			}else {
-				System.out.println("O nome digitado não está na lista");
-				break;
+			repetido++;	
 			}
-			
 		}
+		
+		/*	if(repetido==0) {
+				System.out.println("Não há nomes iguais");	
+				
+			}else {
+				System.out.print("O sexto nome digitado está presente na lista "+repetido+" vezes");
+				
+			}*/
+		
+			System.out.println(repetido==0?"Não há nomes iguais":"O sexto nome digitado está presente na lista "+repetido+" vezes");
+			
 		
 		
 	
